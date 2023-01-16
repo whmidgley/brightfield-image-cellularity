8b96822d048a7be184b819be29def130dec30f94
Will Midgley <w.h.midgley@swansea.ac.uk>
2023-01-16 09:08:54 +0000

    Added code to remove gradient

---
+++ 01_find_gradiant.r
@@ -0,0 +1,115 @@
+# ==========================================================================
+# Clear and load
+# ==========================================================================
+cat("Clear and load!\n")
+
+setwd("C:/Users/william.midgley/Documents/Personal development/Amy's PhD/cell-analysis")
+
+rm(list = ls())
+
+library(tidyverse)
+library(tiff)
+library(grid)
+library(gridExtra)
+library(plot3D)
+options(digits=20)  # this allows R to use decimals up to 20 places
+options(repr.plot.width = 15, repr.plot.height = 20)
+
+
+# ==========================================================================
+# Read images
+# ==========================================================================
+
+m_bf1 <- readTIFF("brightfield-images/bf1.tif")
+
+
+# ==========================================================================
+# Let's have a look
+# ==========================================================================
+
+#dim(m_bf1)
+#
+#grid.raster(m_bf1)
+#
+#d_bf1 <- data.frame(
+#  red   = matrix(m_bf1[,,1], ncol=1),
+#  green = matrix(m_bf1[,,2], ncol=1),
+#  blue  = matrix(m_bf1[,,3], ncol=1)
+#  )
+
+#nrow(unique(d_bf1))
+
+
+#plot(d_bf1$red, d_bf1$green, pch = 16, col=rgb(d_bf1))
+
+# ==========================================================================
+# Check greyscale
+# ==========================================================================
+
+#for (i in 1:nrow(d_bf1)) {
+#	if (d_bf1[i,1] != d_bf1[i,2] | d_bf1[i,1] != d_bf1[i,3] | d_bf1[i,2] != d_bf1[i,3]) {
+#		cat("colour!\n")
+#	}
+#}
+# no response so we can just use 
+
+# ==========================================================================
+# Plot 3D graph
+# ==========================================================================
+
+m_bf1_gs <- m_bf1[,,1]
+#
+#persp3D(z = m_bf1_gs, theta = 120)
+
+
+d_bf1_gs <- data.frame(matrix(m_bf1_gs, ncol=1))
+
+d_bf1_gs <-
+expand.grid(1:nrow(m_bf1_gs), 1:ncol(m_bf1_gs)) %>%
+	data.frame() %>%
+	cbind(d_bf1_gs)
+
+colnames(d_bf1_gs) <- c("x", "y", "z")
+
+fit_bf1_gs <- lm(z ~ x + y, data = d_bf1_gs)
+
+
+fun <- function(i,j) {
+	fit_bf1_gs$coefficients[1] + fit_bf1_gs$coefficients[2]*i + fit_bf1_gs$coefficients[3]*j
+}
+
+rows <- 1:699
+cols <- 1:699
+
+m_plane <- outer(rows,cols,FUN=fun)
+
+# ==========================================================================
+# Let's have a look at that plane
+# ==========================================================================
+
+#persp3D(z = m_plane, theta = 120)
+#
+#d_plane <- data.frame(matrix(m_plane, ncol=1))
+#
+#d_plane_clrd <- cbind(d_plane, d_plane, d_plane)
+#colnames(d_plane_clrd) <- c("red", "green", "blue")
+# 
+#m_plane_clrd <- array(
+#  d_plane_clrd %>%
+#    select(
+#      red,
+#      green,
+#      blue
+#    ) %>%
+#    unlist() %>%
+#    unname(),
+#  dim = dim(m_bf1)
+#)
+#
+#grid.raster(m_plane_clrd)
+
+# ==========================================================================
+# Save plane
+# ==========================================================================
+
+save(m_plane, file ="qsave/m_plane.RData")
--- 02_find_cell_area.R
+++ 02_find_cell_area.R
@@ -20,6 +20,7 @@ options(repr.plot.width = 15, repr.plot.height = 20)
 
 m_bf1 <- readTIFF("brightfield-images/bf1.tif")
 
+load("qsave/m_plane.RData")
 
 # ==========================================================================
 # Let's have a look
@@ -42,46 +43,81 @@ d_bf1 <- data.frame(
 
 
 # ==========================================================================
+# Normalise the image using the plane
+# ==========================================================================
+
+m_bf1_gs <- m_bf1[,,1]
+
+m_bf1_gs_normal <- matrix(m_bf1_gs, ncol=1) - matrix(m_plane, ncol=1) + 0.5
+
+
+# ==========================================================================
+# Have a look
+# ==========================================================================
+
+#persp3D(z = m_bf1_gs_normal, theta = 120)
+
+
+d_bf1_gs_normal <- data.frame(matrix(m_bf1_gs_normal, ncol=1))
+
+d_bf1_normal <- cbind(d_bf1_gs_normal, d_bf1_gs_normal, d_bf1_gs_normal)
+colnames(d_bf1_normal) <- c("red", "green", "blue")
+ 
+m_bf1_normal <- array(
+  d_bf1_normal %>%
+    select(
+      red,
+      green,
+      blue
+    ) %>%
+    unlist() %>%
+    unname(),
+  dim = dim(m_bf1)
+)
+
+grid.raster(m_bf1_normal)
+
+# ==========================================================================
 # k-means clustering
 # ==========================================================================
 
 
-k_bf1 <- kmeans(d_bf1, 4) # separate into 3 colours
+k_bf1_normal <- kmeans(d_bf1_normal, 5) # separate into 3 colours
 
 
 #error <- vector()   # define an empty object to store the error for each cluster
 #for (i in 1:15){
-#  error[i]<-kmeans(d_bf1,i)$tot.withinss 
+#  error[i]<-kmeans(d_bf1_normal,i)$tot.withinss 
 #}
 
 #plot(error)
 
 
-#head(k_bf1)
-
+#head(k_bf1_normal)
+d_bf1_normal_k <- d_bf1_normal
 # Attach cluster labels to our dataframe
-d_bf1$label <- k_bf1$cluster
+d_bf1_normal_k$label <- k_bf1_normal$cluster
 
 # Let's also assign the cluster labels to the cluster colours
-colours1 <- data.frame(k_bf1$centers, c(1:4))
+colours1 <- data.frame(k_bf1_normal$centers, c(1:5))
 colnames(colours1) <- c("red.c","green.c","blue.c","label")
 colours1
 
 
-d_bf1 <- inner_join(d_bf1, colours1 ,by="label")
+d_bf1_normal_k <- inner_join(d_bf1_normal_k, colours1 ,by="label")
 
-#head(d_bf1)
+#head(d_bf1_normal)
 
 
-d_bf1 %>%
+d_bf1_normal_k %>%
 select(red.c,green.c,blue.c) %>%
 unlist() %>%
 unname()
 
 
 
-a_bf1_segmented <- array(
-  d_bf1 %>%
+m_bf1_normal_segmented <- array(
+  d_bf1_normal_k %>%
     select(
       red.c,
       green.c,
@@ -92,4 +128,4 @@ a_bf1_segmented <- array(
   dim = dim(m_bf1)
 )
 
-grid.raster(a_bf1_segmented)
+grid.raster(m_bf1_normal_segmented)
