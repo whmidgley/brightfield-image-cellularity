1435d46efc23f1fd648308b3479adf4ac245e2b1
Will Midgley <w.h.midgley@swansea.ac.uk>
2023-02-20 13:44:16 +0000

    Increase sample size

--- 01_get_cellularity.r
+++ 01_get_cellularity.r
@@ -57,24 +57,62 @@ options(digits=20)  # this allows R to use decimals up to 20 places
 options(repr.plot.width = 15, repr.plot.height = 20)
 
 # ==========================================================================
-# Perform an example k-means
+# Perform example k-means
 # ==========================================================================
 
-m_bf <- suppressWarnings(readImage("bf_example.tif"))
+images <- list.files(path = "brightfield-images", pattern = "tif", recursive = FALSE, full.names = TRUE) 
 
-source("01a_remove_gradient.r")
-source("01b_detect_edges.r")
+# sample 30%
+sample_size <- round(length(images)*0.3)
+samples <- sample(images, size = sample_size)
 
-m_bf_blur <- m_bf_blur/max(m_bf_blur)
-d_bf_blur <- data.frame(matrix(m_bf_blur, ncol=1))
+image_sizes <- c()
+for (j in 1:sample_size) {
+	assign(paste0("m_bf"),
+		suppressWarnings(readImage(paste0(samples[j])))
+		)
 
-k_bf_blur <- kmeans(d_bf_blur, 5)
+	source("01a_remove_gradient.r")
+	source("01b_detect_edges.r")
+	
+	image_sizes[j] <- ncol(m_bf_blur)
 
-d_bf_blur_k <- d_bf_blur
+	assign(paste0("m_sample_blur", j),
+		m_bf_blur
+		)
+}
 
-d_bf_blur_k$label <- k_bf_blur$cluster
+min_size <- min(image_sizes)
 
-colours1 <- data.frame(k_bf_blur$centers, c(1:5))
+d_sample_blur <- c()
+for (j in 1:sample_size) {
+	assign(paste0("m_sample_blur", j),
+		get(paste0("m_sample_blur", j))[1:min_size, 1:min_size]
+		)
+	d_sample_blur[j] <- get(paste0("m_sample_blur", j)) %>% matrix(ncol = 1) %>% as.data.frame()
+}
+
+d_sample_blur_long <- d_sample_blur[1]
+	for (j in 2:sample_size) {
+		assign("d_sample_blur_long",
+			rbind(d_sample_blur_long, d_sample_blur[j]))
+	}
+
+d_sample_blur_long <- cbind(d_sample_blur_long, d_sample_blur_long, d_sample_blur_long)
+
+m_sample_blur <- unlist(d_sample_blur_long) %>% array(dim = c(min_size, sample_size*min_size))
+
+
+m_sample_blur <- m_sample_blur/max(m_sample_blur)
+d_sample_blur <- data.frame(matrix(m_sample_blur, ncol=1))
+
+k_sample_blur <- kmeans(d_sample_blur, 5)
+
+d_sample_blur_k <- d_sample_blur
+
+d_sample_blur_k$label <- k_sample_blur$cluster
+
+colours1 <- data.frame(k_sample_blur$centers, c(1:5))
 colnames(colours1) <- c("centres","label")
 
 centres 		<- colours1$centres
@@ -83,8 +121,6 @@ background 	<- min(colours1$centres)
 # ==========================================================================
 # Calculate cellularities
 # ==========================================================================
-
-images <- list.files(path = "brightfield-images", pattern = "tif", recursive = FALSE, full.names = TRUE) 
 
 cellularities <- data.frame(matrix(nrow=length(images), ncol=2))
 colnames(cellularities) <- c("image_name", "cellularity")
