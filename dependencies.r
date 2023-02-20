aa3a540c85e22689be27729ff8853e2a10094fe3
Will Midgley <w.h.midgley@swansea.ac.uk>
2023-02-20 16:16:20 +0000

    Added a couple packages

--- dependencies.r
+++ dependencies.r
@@ -1,5 +1,5 @@
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(beepr, stringr, BiocManager)
+pacman::p_load(tidyverse, beepr, stringr, BiocManager)
 
 library(BiocManager)
 BiocManager::install("EBImage")
