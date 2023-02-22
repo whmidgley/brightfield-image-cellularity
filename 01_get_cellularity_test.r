d97b52d9a576dbd5b32e7c66ab6a104453207e14
Will Midgley <w.h.midgley@swansea.ac.uk>
2023-02-22 14:30:39 +0000

    Gridsearch

--- 01_get_cellularity_test.r
+++ 01_get_cellularity_test.r
@@ -62,7 +62,7 @@ options(repr.plot.width = 15, repr.plot.height = 20)
 
 images <- list.files(path = "brightfield-images", pattern = "tif", recursive = FALSE, full.names = TRUE) 
 
-images <- sample(images, 20)
+images <- sample(images, 5)
 
 
 cellularities <- data.frame(matrix(nrow=length(images), ncol=2))
@@ -74,19 +74,20 @@ image_names <- sub('.+/(.+)', '\\1', images)
 # Calculate cellularities
 # ==========================================================================
 
-blur_numbers <- c(0:25)
-cut_off_numbers <- c(1:15)
+blur_numbers <- c(0:15)
+cut_off_numbers <- c(1:10)
 
 grid_search <- matrix(nrow = length(blur_numbers), ncol = length(cut_off_numbers))
 
 
-#for (blurno in blur_numbers) {	
-blurno <- 1
+for (blurno in blur_numbers) {
 for (cut_offno in cut_off_numbers) {
 
-blur <- blurno/10
-cut_off <- cut_offno/25
+blur <- blurno/8
+cut_off <- cut_offno/50
 
+auto_cellularities <- data.frame(matrix(ncol = 2, nrow = length(image_names)))
+colnames(auto_cellularities) <- c("name", "cellularity")
 
 for (j in 1:length(images)) {
 	cat("Image",j,"=================\n")
@@ -96,18 +97,20 @@ source("01a_remove_gradient.r")
 source("01b_detect_edges.r")
 source("01c_cut_off.r")
 
-cellularities[j,] <- c(image_names[j], print(computer_cellularity))
+auto_cellularities[j,] <- c(image_names[j], print(computer_cellularity))
 }
 
-write.csv(cellularities, "automated_cellularities.csv", row.names = FALSE)
+write.csv(auto_cellularities, "automated_cellularities.csv", row.names = FALSE)
 
 source("02_get_human_cellularity.r")
 
 mean_error <- mean(cellularities$error, na.rm = TRUE)
 
-gridsearch[blurno, cut_offno] <- mean_error
+grid_search[blurno, cut_offno] <- mean_error
 
-#}
 }
+}
+
+plot(x = cut_off_numbers, y = grid_search[1,]^2)
 
 beep()
